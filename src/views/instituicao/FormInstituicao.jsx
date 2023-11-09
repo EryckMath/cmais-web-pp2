5aed9f63d7e92058e207b8e5e2623c7603ea88b7
Eryck <emfs4@discente.ifpe.edu.br>
2023-11-09 16:03:51 -0300

    attQuinta

--- src/views/instituicao/FormInstituicao.jsx
+++ src/views/instituicao/FormInstituicao.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import InputMask from 'react-input-mask';
-import { useLocation, Link  } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 import MenuSistema from "../../MenuSistema";
 
